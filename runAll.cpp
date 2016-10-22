ccf8bd6e8b414de9d14e9d43f5b627a572fed003
lilileilei <zhanghl86@126.com>
2016-10-22 19:05:37 +0800

    Initial comimit

---
+++ runAll.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+
+class TestGame : public ::testing::Test{
+public:
+  void SetUp(){
+    //code here be executed before test case
+  }
+
+  void TearDown(){
+    //Code here be executed after test case
+  }
+};
+
+TEST(TestGame, startFromHere)
+{
+  //FAIL()<<"start from here!";
+  ASSERT_TRUE(true);
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
